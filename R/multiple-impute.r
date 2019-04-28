8cf5febe79633a7e7bf4bf79b2154c1b841d5459
Michael Kane <kaneplusplus@gmail.com>
2019-04-28 17:40:23 -0400

    Parallel multiple imputation.

--- R/multiple-impute.r
+++ R/multiple-impute.r
@@ -57,6 +57,7 @@ all_rows_imputed <- function(x, imputed_x) {
 
 #' @importFrom simputation impute_rf impute_mf impute_rhd impute_knn
 #' @importFrom equivalent equiv
+#' @importFrom foreach %dopar% registerDoSEQ getDoParName
 #' @export
 multiple_impute.formula <- function(x, form, 
   impute_model = list(imput_rf = simputation::impute_rf, 
@@ -66,11 +67,16 @@ multiple_impute.formula <- function(x, form,
   num_imputes = 5,
   remove_equivalents = TRUE,
   .key = "data") {
+
+  if (is.null(getDoParName())) {
+    registerDoSEQ()
+  }
  
   if (is.function(impute_model)) {
     impute_n_times(x, form, impute_model, num_imputes)
   } else if (is.list(impute_model)) {
-    idata <- foreach(i = seq_along(impute_model), .combine = bind_rows) %do% {
+    idata <- foreach(i = seq_along(impute_model), 
+                     .combine = bind_rows) %dopar% {
       impute_name <- names(impute_model)[i]
       if (!is.null(impute_name)) {
         imput_name <- ""
