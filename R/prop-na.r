acbb660c79be5a444dc18622daa6e8ecb508d555
Michael Kane <kaneplusplus@gmail.com>
2019-04-30 20:51:58 -0400

    Yeah.

--- R/prop-na.r
+++ R/prop-na.r
@@ -5,12 +5,29 @@
 #' @importFrom crayon red yellow
 #' @export
 prop_na <- function(x) {
-  if (!is.vector(x)) {
-    stop(red("Don't know how to get the proportion NA for a object of class ",
-             class(x), ".", sep = ""))
-  }
+  UseMethod("prop_na", x)
+}
+
+prop_na.default <- function(x) {
+  stop(red("Don't know how to get the proportion NA for a object of class ",
+           class(x), ".", sep = ""))
+}
+
+prop_na_vector <- function(x) {
   if (length(x) == 0) {
     warning(yellow("Vector of length 0 passed to prop_na."))
   }
   sum(is.na(x)) / length(x)
+}
+
+prop_na.character <- prop_na.numeric <- prop_na.logical <- 
+  prop_na.factor <- prop_na.list <- prop_na_vector
+
+#' @export
+prop_na.data.frame <- function(x) {
+  ret <- rep(NA_real_, ncol(x))
+  for (j in seq_along(x)) {
+    ret[j] <- prop_na(x[[j]])
+  }
+  ret
 }
